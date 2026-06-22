ca0dd447205203868f9a07bf3bc3b99abe7cd2e3
Aleks Levet <alekslevet@gmail.com>
2026-06-22 02:02:37 +0200

    Workflow build

--- app/src/main/java/com/better/nothing/music/vizualizer/service/AudioCaptureService.java
+++ app/src/main/java/com/better/nothing/music/vizualizer/service/AudioCaptureService.java
@@ -1622,7 +1622,7 @@ public class AudioCaptureService extends Service {
 
         try {
             if (DeviceProfile.getMatrixWidth(mSelectedDevice) > 0) {
-                if (mGMM != null) mGMM.setAppMatrixFrame(frameColors);
+                if (mGMM != null) mGMM.setAppMatrixFrame(frameColors); //Thank you Frank
             } else {
                 mGM.setFrameColors(frameColors);
             }
